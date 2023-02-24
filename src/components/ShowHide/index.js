969d3581003f66a0243c45e1f96cb4a623de32dd
Shubham Arora <108581626+samaroraytb@users.noreply.github.com>
2023-02-24 12:53:13 +0530

    Update index.js

--- src/components/ShowHide/index.js
+++ src/components/ShowHide/index.js
@@ -2,22 +2,68 @@ import {Component} from 'react'
 import './index.css'
 
 class ShowHide extends Component {
+  state = {firstNameStatus: false, lastNameStatus: false}
+
+  firstNameShowcase = () => {
+    this.setState(prevState =>
+      prevState.firstNameStatus === true
+        ? {firstNameStatus: false}
+        : {firstNameStatus: true},
+    )
+  }
+
+  lastNameShowcase = () => {
+    this.setState(prevState =>
+      prevState.lastNameStatus === true
+        ? {lastNameStatus: false}
+        : {lastNameStatus: true},
+    )
+  }
+
   render() {
+    const {firstNameStatus, lastNameStatus} = this.state
+
+    let isShowFirstName = ''
+    if (firstNameStatus === true) {
+      isShowFirstName = (
+        <div className="Show-Hide-container">
+          <p className="name-heading">Joe</p>
+        </div>
+      )
+    }
+
+    let isShowLastName = ''
+    if (lastNameStatus === true) {
+      isShowLastName = (
+        <div className="Show-Hide-container">
+          <p className="name-heading">Jonas</p>
+        </div>
+      )
+    }
+
     return (
       <div className="main-bg-container">
         <h1>Show/Hide</h1>
         <div className="container-show-card">
           <div className="name-card-container">
-            <button type="button">Show/Hide Firstname</button>
-            <div className="Show//Hide-container">
-              <h1>Joe</h1>
-            </div>
+            <button
+              onClick={this.firstNameShowcase}
+              className="button-style"
+              type="button"
+            >
+              Show/Hide Firstname
+            </button>
+            {isShowFirstName}
           </div>
           <div className="name-card-container">
-            <button type="button">Show/Hide Firstname</button>
-            <div className="Show//Hide-container">
-              <h1>Joe</h1>
-            </div>
+            <button
+              onClick={this.lastNameShowcase}
+              className="button-style"
+              type="button"
+            >
+              Show/Hide Lastname
+            </button>
+            {isShowLastName}
           </div>
         </div>
       </div>
